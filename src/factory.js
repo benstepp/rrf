3752547b6a00d731fe2f7e352dec82f70fd28bc9
Benjamin Stepp <me@benstepp.com>
2017-03-01 09:29:29 -0600

    Extract isPlural to utils

--- src/factory.js
+++ src/factory.js
@@ -1,11 +1,8 @@
+import isPlural from './utils/is-plural'
 import plural from './plural-factory'
 import singular from './singular-factory'
 
 export default function factory(component, name) {
   const plural = isPlural(name)
   return plural ? plural(component, name) : singular(component, name)
-}
-
-function isPlural(name) {
-  return name[name.length - 1] === 's'
 }
---
+++ src/utils/is-plural.js
@@ -0,0 +1,3 @@
+export default function isPlural(word) {
+  return name[name.length - 1] === 's'
+}
